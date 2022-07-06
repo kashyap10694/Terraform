75d25be16fe89eb4dd86fe0629dce8fceae6b62b
kashyap10694 <51030657+kashyap10694@users.noreply.github.com>
2022-07-06 14:36:19 +0530

    Update main.tf

--- networking/main.tf
+++ networking/main.tf
@@ -76,7 +76,13 @@ resource "aws_security_group" "web_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
+ 
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   egress {
     from_port   = 0
     to_port     = 0
